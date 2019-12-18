c1ad3236ce20f7d1ce2580c95c73a3b7c0c3e92b
Jack-WangZhe <mrjackzhe@gmail.com>
2019-12-18 21:51:05 +0800

    初始化项目

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,33 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+import './index.less'
+
+export default class Index extends Component {
+
+  config = {
+    navigationBarTitleText: '首页'
+  }
+
+  // 实体对象
+  state = {
+    name: 'Hello Jack'
+  }
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>Hello world!</Text>
+      </View>
+    )
+  }
+}
